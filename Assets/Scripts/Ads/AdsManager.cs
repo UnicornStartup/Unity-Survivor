27bff11b6cc8c1553fe8f52ef7275c2fb9cacae0
Jaime Teixidó || MoONStter Teixi <jteixidof@gmail.com>
2022-03-26 00:37:21 +0100

    Test1

---
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] string _androidAdUnitId = "Interstitial_Android";
+    [SerializeField] string _iOsAdUnitId = "Interstitial_iOS";
+    string _adUnitId;
+
+    void Awake()
+    {
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer) ? _iOsAdUnitId : _androidAdUnitId;
+    }
+
+    public void LoadAd()
+    {
+        Advertisement.Load(_adUnitId, this);
+    }
+
+    public void ShowAd()
+    {
+        Advertisement.Show(_adUnitId, this);
+    }
+
+    public void OnUnityAdsAdLoaded(string adUnitId) { }
+
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+    {
+        DataClass.AD_VIWED = true;
+    }
+}
---
+++ Assets/Scripts/DataClass.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class DataClass
+{
+    public static int CONFIG_COUNT_DOWN_START = 3;
+    public static float CONFIG_COUNT_DOWNT_MAX = 3;
+
+    public static bool IS_CONTINUE = false;
+    public static bool AD_VIWED = false;
+    public static int HIGH_SCORE
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("HIGH_SCORE");
+        }
+        set
+        {
+            PlayerPrefs.SetInt("HIGH_SCORE", value);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static string SETTINGS_LANGUAGE
+    {
+        get
+        {
+            return (PlayerPrefs.GetString("LANGUAGE") != string.Empty) ? PlayerPrefs.GetString("LANGUAGE") : "es";
+        }
+        set
+        {
+            PlayerPrefs.SetString("LANGUAGE", value);
+            PlayerPrefs.Save();
+        }
+    }
+    public static int SETTINGS_VOLUME_MUSIC
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("VOLUME_MUSIC");
+        }
+        set
+        {
+            PlayerPrefs.SetInt("VOLUME_MUSIC", value);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static int SETTINGS_VOLUME_EFFECTS
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("VOLUME_EFFECTS");
+        }
+        set
+        {
+            PlayerPrefs.SetInt("VOLUME_EFFECTS", value);
+            PlayerPrefs.Save();
+        }
+    }
+}
